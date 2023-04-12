38844737ed4f6f85e78b5ae0f8342bfa44eef215
Salah2Eddin <50306707+Salah2Eddin@users.noreply.github.com>
2023-04-12 20:52:17 +0200

    Add student form JS
    Added students are saved to Local Storage

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,19 @@
-function toggle_filters(){
-    let e = document.querySelector("#filters");
+function toggle(selector) {
+    let e = document.querySelector(selector);
     e.classList.toggle("hide");
+}
+
+function validateEmail(email) {
+    const exp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return exp.test(email);
+}
+
+function validatePhoneNumber(phone_number) {
+    const exp = /^(?:\+20|0)?1[0125]\d{8}$/;
+    return exp.test(phone_number);
+}
+
+function validateID(id){
+    const exp = /^[0-9]+$/;
+    return exp.test(id);
 }
