e0e7c345218055923f906c702be4ac2de0111e88
Salah2Eddin <50306707+Salah2Eddin@users.noreply.github.com>
2023-04-14 14:34:53 +0200

    Added Search Filters

--- assets/script.js
+++ assets/script.js
@@ -1,232 +1,337 @@
-if (localStorage.getItem('students') === null) {
-    localStorage.setItem('students', '{}');
+if (localStorage.getItem("students") === null) {
+  localStorage.setItem("students", "{}");
 }
+
+const departments = {
+  general: "General",
+  cs: "Computer Science",
+  it: "Information Technology",
+  is: "Information Systems",
+  ai: "Artificial Intelligence",
+  ds: "Decision Support",
+};
+
+const parametersToShow = ["name", "id", "level", "department", "gpa", "status"];
 
 function toggleClass(selector) {
-    let e = document.querySelector(selector);
-    e.classList.toggle("hide");
+  let e = document.querySelector(selector);
+  e.classList.toggle("hide");
 }
 
-
 function validateEmail(email) {
-    const exp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return exp.test(email);
+  const exp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return exp.test(email);
 }
 
 function validatePhoneNumber(phone_number) {
-    const exp = /^(?:\+20|0)?1[0125]\d{8}$/;
-    return exp.test(phone_number);
+  const exp = /^(?:\+20|0)?1[0125]\d{8}$/;
+  return exp.test(phone_number);
 }
 
 function validateID(id) {
-    const exp = /^[0-9]+$/;
-    return exp.test(id);
+  const exp = /^[0-9]+$/;
+  return exp.test(id);
 }
 
 function validateBirthDate(dob) {
-    const cur = new Date();
-    const date = new Date(dob);
-    return date.getTime() <= cur.getTime();
+  const cur = new Date();
+  const date = new Date(dob);
+  return date.getTime() <= cur.getTime();
 }
 
 function initAddStudent() {
-    initDepartmentDropMenu();
+  initDepartmentDropMenu();
 
-    const students = JSON.parse(localStorage.getItem('students'));
-    const form = document.querySelector("#add-student-form");
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const obj = createObjectOnSubmit(event);
-        if (obj['level'] <= 2) {
-            obj['department'] = 'general';
-        }
-
-        const errors = validateStudent(obj);
-        if (errors.length) {
-            let error_message = "";
-            for (let error of errors)
-                error_message += "\n" + error;
-            showErrorMessage(error_message);
-        } else if (obj['id'] in students) {
-            showErrorMessage("Student with same ID already exists.");
-        } else {
-            form.reset();
-            students[obj['id']] = obj;
-            localStorage.setItem('students', JSON.stringify(students));
-            showSuccessMessage("Student " + obj['name'] + " added")
-        }
-    });
+  const students = JSON.parse(localStorage.getItem("students"));
+  const form = document.querySelector("#add-student-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const obj = createObjectOnSubmit(formData);
+    if (obj["level"] <= 2) {
+      obj["department"] = "general";
+    }
+    const errors = validateStudent(obj);
+    if (errors.length) {
+      let error_message = "";
+      for (let error of errors) error_message += "\n" + error;
+      showErrorMessage(error_message);
+    } else if (obj["id"] in students) {
+      showErrorMessage("Student with same ID already exists.");
+    } else {
+      form.reset();
+      students[obj["id"]] = obj;
+      localStorage.setItem("students", JSON.stringify(students));
+      showSuccessMessage("Student " + obj["name"] + " added");
+    }
+  });
 }
 
 function initEditStudent() {
-    const form = document.querySelector("#edit-student-form");
+  const form = document.querySelector("#edit-student-form");
 
-    const queryString = window.location.search;
-    const searchParams = new URLSearchParams(queryString);
+  const queryString = window.location.search;
+  const searchParams = new URLSearchParams(queryString);
 
-    const students = JSON.parse(localStorage.getItem('students'));
-    for (const [key, value] of Object.entries(students[searchParams.get('id')])) {
-        form.elements[key].value = value;
+  const students = JSON.parse(localStorage.getItem("students"));
+  for (const [key, value] of Object.entries(students[searchParams.get("id")])) {
+    form.elements[key].value = value;
+  }
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const obj = createObjectOnSubmit(formData);
+    const errors = validateStudent(obj);
+    if (obj["level"] <= 2 || obj["department"] === undefined) {
+      obj["department"] = "general";
     }
-
-    initDepartmentDropMenu();
-
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const obj = createObjectOnSubmit(event);
-        const errors = validateStudent(obj);
-        if (errors) {
-            let error_message = "";
-            for (let error of errors)
-                error_message += "\n" + error;
-            showErrorMessage(error_message);
-        } else {
-            students[obj['id']] = obj;
-            localStorage.setItem('students', JSON.stringify(students));
-            showSuccessMessage("Edits to " + obj['name'] + "Saved")
-        }
-
-    });
+    if (errors.length) {
+      let error_message = "";
+      for (let error of errors) error_message += "\n" + error;
+      showErrorMessage(error_message);
+    } else {
+      students[obj["id"]] = obj;
+      localStorage.setItem("students", JSON.stringify(students));
+      showSuccessMessage("Edits to " + obj["name"] + " saved");
+    }
+  });
 }
 
 function initSearchStudent() {
-    const parametersToShow = ['name', 'id', 'level', 'department', 'gpa', 'status'];
-    let e = document.querySelector("#show-filters");
-    e.addEventListener("click", () => {
-        toggleClass("#filters");
-        e.classList.toggle("active-btn");
-    });
+  initFiltersToggle();
 
-    const table = document.querySelector("#students-table");
+  const students = JSON.parse(localStorage.getItem("students"));
+  populateStudentsTable(students);
 
-    let count = 1;
-    const students = JSON.parse(localStorage.getItem('students'));
-    for (const [, student] of Object.entries(students)) {
+  const form = document.querySelector("#search-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-        const row = document.createElement('tr');
-        let cell = document.createElement('td');
-        cell.innerHTML = `${count++}`;
+    const formData = new FormData(event.target);
 
-        row.appendChild(cell);
-        for (const key in parametersToShow) {
-            cell = document.createElement('td');
+    let filtered = filterFromFormData(formData, students);
 
+    populateStudentsTable(filtered);
+  });
+}
 
-            let value = student[parametersToShow[key]];
-            if (typeof (value) === "string") {
-                value = value.charAt(0).toUpperCase() + value.slice(1);
-            }
-            cell.innerHTML = value;
-            row.appendChild(cell);
-        }
+function initActiveStudent() {
+  initFiltersToggle();
 
-        cell = document.createElement('td');
+  const form = document.querySelector("#search-form");
+  const formData = new FormData(form);
 
-        let anchor = document.createElement('a');
-        anchor.href = 'edit.html?id=' + student['id'];
-        anchor.innerText = "Edit";
-        cell.appendChild(anchor);
-        row.appendChild(cell);
+  const students = JSON.parse(localStorage.getItem("students"));
+  populateStudentsTable(filterFromFormData(formData, students));
 
-        cell = document.createElement('td');
-        anchor = document.createElement('a');
-        anchor.href = 'department.html?id=' + student['id'];
-        anchor.innerText = "Change Department";
-        if (student['level'] <= 2) {
-            anchor.style.pointerEvents = 'none';
-            anchor.style.cursor = 'default';
-            anchor.style.color = 'grey';
-        }
-        cell.appendChild(anchor);
-        row.appendChild(cell);
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-        cell = document.createElement('td');
-        anchor = document.createElement('a');
-        anchor.href = '#';
-        anchor.innerText = "Delete";
-        cell.appendChild(anchor);
-        row.appendChild(cell);
+    const formData = new FormData(event.target);
 
-        table.tBodies[0].appendChild(row);
-    }
+    let filtered = filterFromFormData(formData, students);
+
+    populateStudentsTable(filtered);
+  });
 }
 
 function initChangeDepartment() {
+  const queryString = window.location.search;
+  const searchParams = new URLSearchParams(queryString);
 
-    const queryString = window.location.search;
-    const searchParams = new URLSearchParams(queryString);
+  const students = JSON.parse(localStorage.getItem("students"));
+  const student = students[searchParams.get("id")];
 
-    const students = JSON.parse(localStorage.getItem('students'));
-    const student = students[searchParams.get('id')];
-
-    document.getElementById('stud-name').innerHTML = student['name'];
-    document.getElementById('stud-id').innerHTML = student['id'];
-    document.getElementById('stud-level').innerHTML = student['level'];
-    if (student['level'] <= 2)
-        document.getElementById('department-dropdown').disabled = true;
-    else {
-        document.getElementById('department-dropdown').value = student['department'];
-
-        const form = document.getElementById('change-department-form');
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-            student['department'] = document.getElementById('department-dropdown').value;
-
-            students[student['id']] = student;
-            localStorage.setItem('students', JSON.stringify(students));
-
-            showSuccessMessage("Student " + student['name'] + "'s changed successfully!");
-        });
-    }
+  document.getElementById("stud-name").innerHTML = student["name"];
+  document.getElementById("stud-id").innerHTML = student["id"];
+  document.getElementById("stud-level").innerHTML = student["level"];
+  if (student["level"] <= 2) {
+    document.getElementById("department-dropdown").disabled = true;
+  } else {
+    document.getElementById("department-dropdown").value =
+      student["department"];
+    const form = document.getElementById("change-department-form");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      student["department"] = document.getElementById(
+        "department-dropdown"
+      ).value;
+      students[student["id"]] = student;
+      localStorage.setItem("students", JSON.stringify(students));
+      showSuccessMessage(
+        "Student " + student["name"] + "'s department changed successfully!"
+      );
+    });
+  }
 }
 
 function showSuccessMessage(message) {
-    const cont = document.querySelector("#message-modal-container");
+  const cont = document.querySelector("#notification-container");
 
-    cont.classList.remove("hide");
-    cont.classList.remove("error-msg");
-    cont.classList.add("success-msg");
-    cont.querySelector("#message-modal-text").innerHTML = message;
+  cont.classList.remove("hide");
+  cont.classList.remove("error-msg");
+  cont.classList.add("success-msg");
+  cont.querySelector("#notification-text").innerHTML = message;
 }
 
 function initDepartmentDropMenu() {
-    const lvl_menu = document.querySelector("#level-dropdown");
-    const dep_menu = document.querySelector("#department-dropdown");
-    lvl_menu.addEventListener("change", () => {
-        dep_menu.disabled = lvl_menu.selectedIndex < 2;
-    });
+  const lvl_menu = document.querySelector("#level-dropdown");
+  const dep_menu = document.querySelector("#department-dropdown");
+  lvl_menu.addEventListener("change", () => {
+    dep_menu.disabled = lvl_menu.selectedIndex < 2;
+  });
+}
+
+function initFiltersToggle() {
+  let btn = document.querySelector("#show-filters");
+  btn.addEventListener("click", () => {
+    toggleClass("#filters");
+    btn.classList.toggle("active-btn");
+  });
 }
 
 function showErrorMessage(message) {
-    const cont = document.querySelector("#message-modal-container");
+  const cont = document.querySelector("#notification-container");
 
-    cont.classList.remove("hide");
-    cont.classList.remove("success-msg");
-    cont.classList.add("error-msg");
-    cont.querySelector("#message-modal-text").innerText = message;
+  cont.classList.remove("hide");
+  cont.classList.remove("success-msg");
+  cont.classList.add("error-msg");
+  cont.querySelector("#notification-text").innerText = message;
 }
 
 function validateStudent(student) {
-    let errors = [];
-    if (!validateID(student['id'])) {
-        errors.push("Invalid student ID!");
-    }
-    if (!validateEmail(student['email'])) {
-        errors.push("Invalid Email!")
-    }
-    if (!validatePhoneNumber(student['phone'])) {
-        errors.push("Invalid phone number!")
-    }
-    if(!validateBirthDate(student['dob']))
-        errors.push("Invalid birth date!")
-    return errors;
+  let errors = [];
+  if (!validateID(student["id"])) {
+    errors.push("Invalid student ID!");
+  }
+  if (!validateEmail(student["email"])) {
+    errors.push("Invalid Email!");
+  }
+  if (!validatePhoneNumber(student["phone"])) {
+    errors.push("Invalid phone number!");
+  }
+  if (!validateBirthDate(student["dob"])) {
+    errors.push("Invalid birth date!");
+  }
+  return errors;
 }
 
-function createObjectOnSubmit(submitEvent) {
-    const formData = new FormData(submitEvent.target);
-    const obj = {};
-    for (const [key, value] of formData.entries()) {
-        obj[key] = value;
+function createObjectOnSubmit(formData) {
+  const obj = {};
+  for (const [key, value] of formData.entries()) {
+    obj[key] = value;
+  }
+  return obj;
+}
+
+function populateStudentsTable(students) {
+  let count = 1;
+  const table = document.querySelector("#students-table");
+
+  table.tBodies[0].innerHTML = "";
+
+  for (const [, student] of Object.entries(students)) {
+    const row = document.createElement("tr");
+    let cell = document.createElement("td");
+    cell.innerHTML = `${count++}`;
+
+    row.appendChild(cell);
+    for (const key in parametersToShow) {
+      cell = document.createElement("td");
+
+      const param = parametersToShow[key];
+      let value = student[param];
+      if (param === "department") {
+        value = departments[value];
+      } else if (param === "status") {
+        if (value === "active") {
+          value = '<input type="checkbox" checked disabled>';
+        } else {
+          value = '<input type="checkbox" disabled>';
+        }
+      }
+      cell.innerHTML = value;
+      row.appendChild(cell);
     }
-    return obj;
+
+    cell = document.createElement("td");
+
+    let anchor = document.createElement("a");
+    anchor.href = "edit.html?id=" + student["id"];
+    anchor.innerText = "Edit";
+    cell.appendChild(anchor);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    anchor = document.createElement("a");
+    anchor.href = "department.html?id=" + student["id"];
+    anchor.innerText = "Change Department";
+    if (student["level"] <= 2) {
+      anchor.setAttribute("disabled", "");
+    }
+    cell.appendChild(anchor);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    anchor = document.createElement("a");
+    anchor.href = "#";
+    anchor.innerText = "Delete";
+    cell.appendChild(anchor);
+    row.appendChild(cell);
+
+    table.tBodies[0].appendChild(row);
+  }
+}
+
+function filterFromFormData(formData, students) {
+  let filtered = [];
+
+  const name = formData.get("name");
+  const id = formData.get("id");
+  const lgpa = formData.get("l-gpa");
+  const rgpa = formData.get("r-gpa");
+  const level = formData.get("level");
+  const status = formData.get("status");
+
+  const nameRegex = new RegExp("\\b" + name + "\\b", "i");
+  const idRegex = new RegExp("^" + id);
+
+  for (const [, student] of Object.entries(students)) {
+    let good = true;
+
+    if (name) {
+      good &= nameRegex.test(student["name"]);
+    }
+
+    if (id) {
+      good &= idRegex.test(student["id"]);
+    }
+
+    good &= !level || level == student["level"];
+    good &= !status || status == student["status"];
+
+    good &= student["gpa"] >= lgpa && student["gpa"] <= rgpa;
+
+    if (good) {
+      filtered.push(student);
+    }
+  }
+
+  const sort_by = formData.get("sort-by");
+  if (sort_by == "name-asc") {
+    filtered.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
+  } else if (sort_by == "name-desc") {
+    filtered.sort((a, b) => (a.name > b.name ? -1 : b.name > a.name ? 1 : 0));
+  } else if (sort_by == "id-asc") {
+    filtered.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0));
+  } else if (sort_by == "id-desc") {
+    filtered.sort((a, b) => (a.id > b.id ? -1 : b.id > a.id ? 1 : 0));
+  } else if (sort_by == "gpa-asc") {
+    filtered.sort((a, b) => (a.gpa > b.gpa ? 1 : b.gpa > a.gpa ? -1 : 0));
+  } else if (sort_by == "gpa-desc") {
+    filtered.sort((a, b) => (a.gpa > b.gpa ? -1 : b.gpa > a.gpa ? 1 : 0));
+  }
+
+  return filtered;
 }
